515267acd0c15c0a794363785c5ea2acf3253245
dinta0623 <aldenxxx12345@gmail.com>
2020-12-14 06:40:09 +0800

    updated .gitignore

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,4 @@
 const Shard = require('../dist')
-jest.mock('../dist')
-
-beforeEach(() => {
-  // Clear all instances and calls to constructor and all methods:
-  Shard.mockClear()
-})
 
 it('run each shard', () => {
   const theClass = new Shard()
